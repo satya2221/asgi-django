006ac8a687bd000c8a90e6b627df829fed76e6da
satya2221 <satyaghifari12@gmail.com>
2025-04-12 11:12:58 +0700

    setup routing for http & websocket

--- core/settings.py
+++ core/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
 
+CHANNEL_LAYERS = {
+	"default": {
+		"BACKEND": "channels.layers.InMemoryChannelLayer"
+	}
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
--- core/urls.py
+++ core/urls.py
@@ -21,3 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notifications.urls')),
 ]
+
+websocket_urlpatterns = [
+    path('ws/notifications/'. NotificationConsumer.as_asgi())
+]
